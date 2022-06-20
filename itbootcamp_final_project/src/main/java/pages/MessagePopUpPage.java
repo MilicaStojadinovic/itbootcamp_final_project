b080fb428483bd54bf699bc13926f6215b9f0b4c
Milica Stojadinovic <mstojadinovic995@gmail.com>
2022-06-20 18:53:36 +0200

    BasicTest

--- itbootcamp_final_project/src/main/java/pages/MessagePopUpPage.java
+++ itbootcamp_final_project/src/main/java/pages/MessagePopUpPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class MessagePopUpPage {
     private WebDriver driver;
 
-
-
-
+    public MessagePopUpPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
