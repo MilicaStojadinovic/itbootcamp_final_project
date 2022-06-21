d6455cb746702e902fe5e58f93401b66bf5cd564
Milica Stojadinovic <mstojadinovic995@gmail.com>
2022-06-21 16:55:42 +0200

    test#2

--- itbootcamp_final_project/src/main/java/tests/SignUpTests.java
+++ itbootcamp_final_project/src/main/java/tests/SignUpTests.java
@@ -13,4 +13,23 @@ public class SignUpTests extends BasicTest{
                         .contains("/signup"),
                 "[ERROR] Page url does not contains '/signup'!");
     }
+    @Test(priority = 2)
+    public void checksInputTypes (){
+        signupPage.getSignMeUpButton().click();
+        Assert.assertEquals(
+                signupPage.getEmailInput().getAttribute("type"),
+                "email",
+                "[ERROR] Input email is not type 'email'!");
+
+        Assert.assertEquals(
+                signupPage.getPasswordInput().getAttribute("type"),
+                "password",
+                "[ERROR] Input password is not type 'password'!");
+        Assert.assertEquals(
+                signupPage.getConfirmPasswordInput().getAttribute("type"),
+                "password",
+                "[ERROR] Input confirm password is not type 'password'!");
+
+    }
+    
 }
